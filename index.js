a1048888a8378cfa63966ad9d1940badd884badf
Zach Bischmann <zachbischmann@Zachs-MacBook-Pro.local>
2025-06-09 14:04:14 -0500

    Add POST route to handle form submissions and create new pet records

--- index.js
+++ index.js
@@ -40,52 +40,23 @@ app.get('/update-cobj', (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+app.post('/update-cobj', async (req, res) => {
+    const { pet_name, pet_type, pet_bio } = req.body;
+    const endpoint = 'https://api.hubapi.com/crm/v3/objects/pets';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
+    const data = {
+        properties: { pet_name, pet_type, pet_bio }
+    };
+    try {
+        await axios.post(endpoint, data, { headers });
+        res.redirect('/');
+    } catch (err) {
         console.error(err);
+        res.send('Error creating record');
     }
-
 });
-*/
 
-
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
