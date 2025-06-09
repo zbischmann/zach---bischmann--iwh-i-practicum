2143e6d6306aed9c72e7f680c0beadcc89360bac
Zach Bischmann <zachbischmann@Zachs-MacBook-Pro.local>
2025-06-09 13:58:58 -0500

    Implement GET route for homepage to fetch and render pet records

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,12 +8,29 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// * Reads the token from .env file
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const endpoint = 'https://api.hubapi.com/crm/v3/objects/pets';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        const resp = await axios.get(endpoint, {
+            headers,
+            params: { properties: 'pet_name,pet_type,pet_bio' }
+        });
+        const records = resp.data.results;
+        res.render('homepage', { title: 'Pet List', records });
+    } catch (error) {
+        console.error(error);
+        res.send('Error loading records');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
